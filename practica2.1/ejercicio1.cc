3b6d893581b1ad0cd2d3bb9847ef11d890b72140
MetalCarlosR <carome06@ucm.es>
2021-05-11 22:36:24 +0200

    Ejercicio1

--- practica2.1/ejercicio1.cc
+++ practica2.1/ejercicio1.cc
@@ -1,9 +1,47 @@
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
 
 
-int main(int argc, char** argv){
-
+int main(int argc, char* argv[]){
+    if(argc < 2)
+    {
+        printf("Please introduce an address. %s domain.xx / 100.0.0.0",argv[0]);
+        return 1;
+    }
     
+   addrinfo* info;
+   int check;
+
+   check = getaddrinfo(argv[1],NULL,NULL,&info);
+    
+    if(check != 0){
+        if(check == EAI_SYSTEM){
+            perror("getaddrinfo");
+        }
+        else
+        {
+            fprintf(stderr , "Error using getaddrinfo: %s\n",gai_strerror(check));
+        }
+        return -1;
+    }
+
+    addrinfo* it;
+    
+    for(it = info; it != NULL; it = it->ai_next){
+        char hostname[1025];
+        check = getnameinfo(it->ai_addr, it->ai_addrlen, hostname, 1025, NULL, 0 , NI_NUMERICHOST);
+        if(check != 0){
+            fprintf(stderr, "Error using getnameinfo: %s\n", gai_strerror(check));
+            continue;
+        }
+        if(hostname != "\0"){
+            printf("Host: %s     Family: %d      SocketType: %d\n", hostname,it->ai_family,it->ai_socktype);
+        }
+    }
+    freeaddrinfo(info);
+
+    return 0;
 }
